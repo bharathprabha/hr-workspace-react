24f6298f2a3bb2fc53ec82a28f51b36a844bdbd8
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-08-07 10:56:22 +0530

    Redesign the  layout.

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -1,10 +1,5 @@
 import React from "react";
 import "./Workspace.css";
-import { EmployeeDetail } from "./workspacecomp/employeedetail/EmployeeDetail";
 export const Workspace = () => {
-  return (
-    <div className="workspace">
-      <EmployeeDetail />
-    </div>
-  );
+  return <div className="workspace">hello</div>;
 };
